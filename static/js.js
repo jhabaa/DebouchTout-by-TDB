92c5d03b770660bd760c12d360a75456025d8b11
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2023-11-24 18:35:09 +0100

    main Page is finished

--- static/js.js
+++ static/js.js
@@ -2,9 +2,20 @@ let burger = document.querySelector('.burger');
 let bugerChid1 = burger.firstElementChild;
 let bugerChid2 = burger.lastElementChild;
 let bar = document.querySelector('#navbar');
+let navBarOpacity = document.querySelector('.back');
 
 burger.addEventListener('click', function () {
     bugerChid2.classList.toggle('active');
     bugerChid1.classList.toggle('active2');
     bar.classList.toggle('extend');
 });
+
+// Get scroll level
+let scroll = document.querySelector('#body');
+//Output scrollY value
+window.onscroll = function () {
+    console.log(scrollY);
+    // Change navbar opacity
+        navBarOpacity.style.opacity = scrollY / 100;
+    
+}

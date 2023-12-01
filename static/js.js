8363ab3fd5008bb0191061c7bcbb77404b4ccc44
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2023-12-01 03:47:50 +0100

    30nov

--- static/js.js
+++ static/js.js
@@ -8,9 +8,41 @@ let logo = document.querySelector('#logo');
 let header_links = document.querySelectorAll('.header_link');
 let articles_page = document.querySelector('#articles');
 let current_page = document.querySelector('#body').dataset.page;
+let services_cards = document.querySelectorAll('.service_carrousel_item');
+let services_index_show = window.innerWidth > 768 ? [0,1,2] : [0];
+let service_carrousel = document.querySelector('.service_carrousel');
+let carrousel_right_btn = document.querySelector('#btn-right');
+let carrousel_left_btn = document.querySelector('#btn-left');
+let carrousel = document.querySelector('#carrousel-1');
+//reviews
+let reviews = document.querySelectorAll('.review');
 if (current_page == "article") {
     var image_background = document.querySelector('#article_background');
 }
+console.log("Values are: ", current_page, window.innerWidth);
+
+//MARK: - Carrousel
+//MARK: - Services carrousel
+var index = 0;
+function moveCarrouselLeft() {
+    carrousel.appendChild(services_cards[index]);
+    index ++;
+    if (index == services_cards.length) {
+        index = 0;
+    }
+}
+function moveCarrouselRight() {
+    if (index <= 0) {
+        index = services_cards.length - 1;
+    }
+    console.log(index);
+    carrousel.insertBefore(services_cards[index], services_cards[0]);
+    index --;
+    
+}
+
+
+
 
 /*
 burger.addEventListener('click', function () {
@@ -26,10 +58,40 @@ let scroll = document.querySelector('#body');
 //MARK: - Depending of the page we are on, we change the navbar links colors
 switch (current_page) {
     case "home":
+        carrousel_left_btn.addEventListener('click', function () {
+            //console.log("left");
+            //console.log(services_index_show);
+            //add last element to the beginning
+            //let removedNode = carrousel.removeChild(services_cards[0]);
+            moveCarrouselLeft();
+        });
+        carrousel_right_btn.addEventListener('click', function () {
+            //console.log("right");
+            //console.log(services_index_show);
+            //add last element to the beginning
+            //let removedNode = carrousel.removeChild(services_cards[0]);
+            moveCarrouselRight();
+        });
         
+        //MARK: - Automatic carrousel every 5 seconds
+        setInterval(function () {
+            moveCarrouselLeft();
+        }, 5000);
         header_links.forEach(function (item) {
             item.classList.color = "white";
         });
+        //always move reviews to the left
+//move cards to the left in async
+let cardWidth = reviews[0].offsetWidth;
+anime({
+    targets: reviews,
+    translateX: ((window.innerWidth / cardWidth) + ((cardWidth * reviews.length) - (window.innerWidth/cardWidth)) - window.innerWidth) * -1,
+    loop: true,
+    //delay: anime.stagger(100),
+    duration: 50000,
+    easing: 'easeInOutSine',
+    direction: 'alternate',
+});
         break;
     case "articles":
         navBarOpacity.style.opacity = 0.6;
@@ -82,5 +144,10 @@ window.onscroll = function () {
         video.style.opacity = 1 - scrollY / 1000;
     }
 }
+
+
+
+//if review is out of screen, move it to the right
+
 
 
